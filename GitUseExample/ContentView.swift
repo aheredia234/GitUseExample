9076214c7c83aab819a40e2e424d3800c31250b0
aheredia234 <42586731+aheredia234@users.noreply.github.com>
2020-08-30 18:20:06 -0500

    Initial Commit

---
+++ GitUseExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitUseExample
+//
+//  Created by Angela Heredia on 8/30/20.
+//  Copyright © 2020 Angela Heredia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
